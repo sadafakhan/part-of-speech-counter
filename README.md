0ef3d4ea753d1049e287593458c021f804730cd5
Sadaf A. Khan <sadaf.alis.khan@gmail.com>
2023-06-17 18:43:14 -0700

    Refined stacks to count NPs while tracking depth

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 A program that counts the number of syntactic constituent types that occur in an annotated corpus. 
 Stacks are employed to keep track of constituents that are defined by a nested structure. For example, ditransitive verb phrases are counted by counting verb phrases followed by two nested noun phrases, whereas intransitive verb phrases are counted by counting verb phrases with no nested children. 
 
-To run from terminal: 
+Args:
+* dir (str): path to the desired directory.
 
+Returns:
+* output (str): formatted table listing count of each constituency type.
+
+To run: 
 ```
 python3 ./main.py data > output
 ```
--- main.py
+++ main.py
@@ -1,68 +1,37 @@
-'''
-Sadaf Khan, Project 1, LING 473, 08/07/2021
-This program takes a directory name as input, and returns a table listing the number of given constituents types
-within the tree-formatted files contained in the directory.
-
-Args:
-    dir (str): path to the desired directory.
-
-Returns:
-    output (str): formatted table listing count of each constituency type.
-'''
-
 from collections import deque
 import os
 import sys
-
 working_dir = sys.argv[1]
-counts = {'(S': 0, '(VP': 0, '(NP': 0}
-structs = []
 
-# extract counts for sentences, verb phrases, and noun phrases 
+counts = {'(S': 0, '(VP': 0, '(NP': 0, "dtv": 0, "itv": 0}
 for file in os.listdir(working_dir):
     with open(os.path.join(working_dir, file), 'r') as f:
-        parse = ""
-        content = f.read()
-        content = content.replace(')', ' ) ')
-        for pos in content.split():
+        depth = deque()
+        parse = f.read()
+        parse = parse.replace(')', ' ) ')
+        
+        for pos in parse.split():
             if pos in counts: 
                 counts[pos] +=1 
-            if pos[0] == "(" or pos == ")": 
-                parse += pos + " "
-        structs.append(parse)
-
-depth = deque()
-vps = [] 
-
-# extract all verb phrases and their nested constituents 
-for doc in structs: 
-    in_vp = False
-    vp = ""
-    for pos in doc.split(): 
-        if pos == "(VP": 
-            depth.append(0)
-            in_vp = True
-
-        if in_vp:
-            vp += pos + " "
-            if pos != "(VP" and pos != ")": 
-                depth.append(0)
-
-            if pos == ")": 
-                depth.pop()
-    
-        if len(depth) == 0: 
-            in_vp = False 
-
-    vps.append(vp)
-
-dtv = 0 
-itv = 0 
-for vp in vps: 
-    if "(VP (NP ) (NP ) )" in vp: 
-        dtv += 1
             
-output = "Sentence\t" + str(counts['(S']) + "\n" + "Noun Phrase\t" + str(counts['(NP']) + "\n" + "Verb Phrase\t" + str(counts['(VP']) + "\n" + "Ditransitive Verb Phrase\t" + str(dtv) + "\n" + "Intransitive Verb Phrase\t" + str(itv)
+            if pos[0] == "(": 
+                if pos == "(VP": 
+                    depth.append([])
+                elif pos == "(NP": 
+                    if type(depth[-1]) == list: 
+                        depth[-1].append(0)
+                    depth.append(0)
+                else: 
+                    depth.append(0)            
+            
+            if pos == ")": 
+                if len(depth) != 0: 
+                    np = depth.pop()
+                    if type(np) == list: 
+                        if len(np) == 0: 
+                            counts["itv"] += 1 
+                        if len(np) == 2: 
+                            counts["dtv"] += 1 
+
+output = "Sentence\t" + str(counts['(S']) + "\n" + "Noun Phrase\t" + str(counts['(NP']) + "\n" + "Verb Phrase\t" + str(counts['(VP']) + "\n" + "Ditransitive Verb Phrase\t" + str(counts['dtv']) + "\n" + "Intransitive Verb Phrase\t" + str(counts["itv"])
 print(output)
-# + \ "Ditransitive Verb Phrase\t" + str(counts['dtv']) + "\n" + "Intransitive Verb Phrase\t" + str(counts['itv']) + "\n"
-    
